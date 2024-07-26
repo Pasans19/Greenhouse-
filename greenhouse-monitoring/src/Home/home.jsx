98d06678b3f8b84b32451216a3a8e30a48c09f3d
Akila Nirmal <akilanirmalzz4352@gmail.com>
2024-07-26 23:06:45 +0530

    add_chart

--- greenhouse-monitoring/src/Home/home.jsx
+++ greenhouse-monitoring/src/Home/home.jsx
@@ -3,13 +3,14 @@ import React from 'react';
 import MyNavbar from '../Components/molecules/navbar/navbar';
 import CarouselComponent from '../Components/molecules/carousel/carousel';
 import FeaturesCard from '../Components/molecules/card/card';
-
+import Chart from '../charts/Charts';
 const Home = () => {
   return (
     <div>
       <MyNavbar/>
       <CarouselComponent/>
       <FeaturesCard/>
+      <Chart/>
     </div>
   );
 };
